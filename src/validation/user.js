b1b1ef6026686863062cf2f94764cb9d580ee2ab
diowartaz <53496307+diowartaz@users.noreply.github.com>
2023-01-08 11:42:39 +0100

    username validator in auth

--- src/validation/user.js
+++ src/validation/user.js
@@ -3,8 +3,8 @@ const joi = require("joi");
 function newUserValidation(body) {
   const newUserValidationSchema = joi.object({
     email: joi.string().email().trim().required(),
-    username: joi.string().min(2).max(20).trim().required(),
-    password: joi.string().min(2).max(20).trim().required(),
+    username: joi.string().min(3).max(15).trim().required(),
+    password: joi.string().min(8).max(30).trim().required(),
   });
   return newUserValidationSchema.validate(body);
 }
